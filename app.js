2b8facfb87d0199f338421c89d33af5f4b3f1a13
jinPark <booster.coffee.juice@gmail.com>
2021-03-22 04:11:00 +0900

    commit_002

--- app.js
+++ app.js
@@ -3,25 +3,32 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
 
 const app = express();
 
-const handleHome = (req,res) => res.send('hello from home!');
-const handleProfile = (req,res) => res.send("You are on my profile");
-
-
 app.use(cookieParser());
-app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile",handleProfile)
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
 
+
+
+// MVC는 패턴 또는 스트럭쳐 같은 것임
+// M Model - data
+// V View - how does the data look
+// C Control - funciton that looks for the data
 
 
 
